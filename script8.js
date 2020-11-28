62f947357939dd3a7e8ff6be7ab4129667612688
hiro-eng <69486574+hiro-eng@users.noreply.github.com>
2020-11-29 07:19:03 +0900

    Update script8.js

--- script8.js
+++ script8.js
@@ -10,10 +10,39 @@ $(document).ready(function(){
     $("#T1").html("<i>ゆっくり過ごそう、ゆっくりね</i>").css("color","green");
 
   });
-  
+
+
   $(".header-list").click(function(){
        $(".header-list").html("<b>ikite2021@gmail.com</b>");
 
+});
+
+  $("#teck").hover(
+    function(){
+   $("#teck").text(" >>>>>Welcome");
+ },function(){
+    $("#teck").text(" by Teck-Teck World");
+ });
+
+ $("h2").hover(
+   function(){
+  $("h2").html("あなたは決して一人ぼっちではありません<br>話を聞かせてください");
+},function(){
+   $("h2").html("もしあなたが自殺を考えたらあなたの身の回りには多くの相談窓口があります<br>そこに相談しましょう");
+});
+
+$("#btn1").hover(
+  function(){
+ $("#btn1").text("準備中");
+},function(){
+  $("#btn1").text("LINE窓口はこちら");
+});
+
+$("#btn2").hover(
+  function(){
+ $("#btn2").text("CLICK!");
+},function(){
+  $("#btn2").text("twitter窓口はこちら");
 });
 
 })
