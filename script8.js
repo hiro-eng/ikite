7f7498966b54be8a95221c3c95d0e0c3c0f10149
hiro-eng <69486574+hiro-eng@users.noreply.github.com>
2020-11-29 12:07:59 +0900

    Update script8.js

--- script8.js
+++ script8.js
@@ -1,6 +1,6 @@
 
 if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
-    location.href = '//';
+    location.href = '/ikitemb/';
 }
 
 
